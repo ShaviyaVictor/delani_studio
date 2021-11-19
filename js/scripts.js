f103a94a1fe97ad2368c21a4a0f842b66052bb55
ShaviyaVictor <shaviyavictor@gmail.com>
2021-11-19 17:56:38 +0300

    Write jQuery scripts down

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,37 @@
 $(document).ready(function(){
   
   $("#designtag").click(function(){
-    $("#designinfo").toggle();
-    $("#designtag").toggle();
+    $("#designinfo").toggle('1500');
+    $("#designtag").toggle('1500');
   });
 
   $("#designinfo").click(function(){
-    $("#designtag").toggle();
-    $("#designinfo").toggle();
+    $("#designtag").toggle('1500');
+    $("#designinfo").toggle('1500');
+  });
+});
+
+$(document).ready(function(){
+
+  $("#developtag").click(function(){
+    $("#developinfo").toggle('1500');
+    $("#developtag").toggle('1500');
+  });
+
+  $("#developinfo").click(function(){
+    $("#developtag").toggle('1500');
+    $("#developinfo").toggle('1500');
+  });
+});
+
+$(document).ready(function(){
+  $("#productag").click(function(){
+    $("#productinfo").toggle('1500');
+    $("#productag").toggle('1500');
+  });
+
+  $("#productinfo").click(function(){
+    $("#productag").toggle('1500');
+    $("#productinfo").toggle('1500');
   });
 });
