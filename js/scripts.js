6c99ec1e5365b52c06754d0a522ebaa7af1cb960
ShaviyaVictor <shaviyavictor@gmail.com>
2021-11-20 07:22:31 +0300

    Write scripts for the mouseover and mouseout effects

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@
 });*/
 
 $(document).ready(function(){
-  
   $("#designtag").click(function(){
     $("#designinfo").toggle('1500');
     $("#designtag").toggle('1500');
@@ -16,7 +15,6 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-
   $("#developtag").click(function(){
     $("#developinfo").toggle('1500');
     $("#developtag").toggle('1500');
@@ -37,5 +35,79 @@ $(document).ready(function(){
   $("#productinfo").click(function(){
     $("#productag").toggle('1500');
     $("#productinfo").toggle('1500');
+  });
+});
+
+//    hover effects scripts
+$(document).ready(function(){
+  
+  $("#image4").mouseover(function(){
+    $("#work4").show(1200);
+  });
+  $("#image4").mouseout(function(){
+    $("#work4").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#image3").mouseover(function(){
+    $("#work3").show(1200);
+  });
+  $("#image3").mouseout(function(){
+    $("#work3").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#image2").mouseover(function(){
+    $("#work2").show(1200);
+  });
+  $("#image2").mouseout(function(){
+    $("#work2").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#image1").mouseover(function(){
+    $("#work1").show(1200);
+  });
+  $("#image1").mouseout(function(){
+    $("#work1").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#image5").mouseover(function(){
+    $("#work5").show(1200);
+  });
+  $("#image5").mouseout(function(){
+    $("#work5").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#image6").mouseover(function(){
+    $("#work6").show(1200);
+  });
+  $("#image6").mouseout(function(){
+    $("#work6").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#image7").mouseover(function(){
+    $("#work7").show(1200);
+  });
+  $("#image7").mouseout(function(){
+    $("#work7").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#image8").mouseover(function(){
+    $("#work8").show(1200);
+  });
+  $("#image8").mouseout(function(){
+    $("#work8").hide();
   });
 });
